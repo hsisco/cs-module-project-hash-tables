e8334e8ab2792cd4e06efb223ff6b419e1236535
hsisco <hysensisco@gmail.com>
2020-08-03 16:30:08 -0400

    Day 1 MVP

--- hashtable/hashtable.py
+++ hashtable/hashtable.py
@@ -7,32 +7,23 @@ class HashTableEntry:
         self.value = value
         self.next = None
 
-
 # Hash table can't have fewer than this many slots
 MIN_CAPACITY = 8
 
 
 class HashTable:
-    """
-    A hash table that with `capacity` buckets
-    that accepts string keys
-
-    Implement this.
-    """
-
     def __init__(self, capacity):
-        # Your code here store array here
-
+        if capacity > MIN_CAPACITY:
+            self.capacity = capacity
+        else:
+            self.capacity = MIN_CAPACITY
+        self.table = [None] * capacity
 
     def get_num_slots(self):
         """
         Return the length of the list you're using to hold the hash
         table data. (Not the number of items stored in the hash table,
         but the number of slots in the main list.)
-
-        One of the tests relies on this.
-
-        Implement this.
         """
         # Your code here
 
@@ -40,30 +31,24 @@ class HashTable:
     def get_load_factor(self):
         """
         Return the load factor for this hash table.
-
-        Implement this.
         """
         # Your code here
 
 
-    def fnv1(self, key):    # choose this....
-        """
-        FNV-1 Hash, 64-bit
-
-        Implement this, and/or DJB2.
-        """
-
-        # Your code here
+    # def fnv1(self, key):
+    #     """
+    #     FNV-1 Hash, 64-bit
+    #     """
+    #     # Your code here
 
 
-    def djb2(self, key):    #... or this
+    def djb2(self, key):
         """
         DJB2 hash, 32-bit
-
-        Implement this, and/or FNV-1.
         """
-        # Your code here
-
+        hash = 5281
+        for b in key:
+            hash = ((hash << 5) + hash) + ord(b)
 
     def hash_index(self, key):
         """
@@ -76,42 +61,31 @@ class HashTable:
     def put(self, key, value):
         """
         Store the value with the given key.
-
         Hash collisions should be handled with Linked List Chaining.
-
-        Implement this.
         """
-        # Your code here
-
+        index = self.hash_index(key)
+        self.table[index] = value
 
     def delete(self, key):
         """
         Remove the value stored with the given key.
-
         Print a warning if the key is not found.
-
-        Implement this.
         """
-        # Your code here
-
+        index = self.hash_index(key)
+        self.table[index] = None
 
     def get(self, key):
         """
         Retrieve the value stored with the given key.
-
         Returns None if the key is not found.
-
-        Implement this.
         """
-        # Your code here
-
+        index = self.hash_index(key)
+        return self.table[index]
 
     def resize(self, new_capacity):
         """
         Changes the capacity of the hash table and
         rehashes all key/value pairs.
-
-        Implement this.
         """
         # Your code here
 
