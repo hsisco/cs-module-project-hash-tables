73383aa422b85e26c47ef11d2bc989016345e03c
hsisco <hysensisco@gmail.com>
2020-08-06 16:47:48 -0400

    MVP 3

--- applications/lookup_table/lookup_table.py
+++ applications/lookup_table/lookup_table.py
@@ -15,12 +15,10 @@ def slowfun(x, y):
     Rewrite slowfun_too_slow() in here so that the program produces the same
     output, but completes quickly instead of taking ages to run.
     """
-    v = math.pow(x, y)
-    v = math.factorial(v)
-    v //= (x + y)
-    v %= 982451653
+    if (x, y) not in cache:
+        cache[(x, y)] = slowfun_too_slow(x, y)
 
-    return v
+    return cache[(x, y)]
 
 
 # Do not modify below this line!
